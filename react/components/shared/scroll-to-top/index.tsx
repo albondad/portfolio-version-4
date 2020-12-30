be3223cf751b734d7495d74c1546f37744c61bbd
Al Bondad <albondad@gmail.com>
2020-12-30 16:01:54 -0600

    Fix issue with useEffect params

--- react/components/shared/scroll-to-top/index.tsx
+++ react/components/shared/scroll-to-top/index.tsx
@@ -2,9 +2,10 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export const ScrollToTop = () => {
+    
     useEffect(() => {
         window.scrollTo(0, 0)
-    }, useLocation().pathname)
+    }, [useLocation().pathname])
 
     return null;
 }
