086922b6f4035f4e0a90a77ba48f57ec338d7a34
Al Bondad <albondad@gmail.com>
2020-12-30 13:59:20 -0600

    Add express erver and yarn scripts

---
+++ express/server.ts
@@ -0,0 +1,19 @@
+const express = require('express');
+const application = express();
+const port =  process.env.PORT || 8000;
+const path = require('path');
+
+application.get('/bundle.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/bundle.js'))
+});
+
+application.get('/images/:id', (req, res) => {
+    const name = req.params.id;
+    res.sendFile(path.join(__dirname, `../public/images/${name}`))
+});
+
+application.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
+application.listen(port)
